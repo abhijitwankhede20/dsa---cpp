59494578ed6752645ce94e2f7ea3a952385bdb5f
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-19 22:12:06 +0530

    Add solution for removing duplicates from sorted array
    Implemented a two-pointer approach to remove duplicates from a sorted array in-place.

--- Arrays/Two_Pointers/Remove_Duplicates_from_Sorted_Array.cpp
+++ Arrays/Two_Pointers/Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,1 +1,21 @@
+// LeetCode 26 - Remove Duplicates from Sorted Array
+// Approach: Two Pointer
+// TC: O(n)
+// SC: O(1)
 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+
+        for(int j = 1; j < n; j++) {
+            if(nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+};

13c9704b560364d79b12cb3ae28ded7e3ca69422
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-19 22:20:05 +0530

    Add solution for Container With Most Water problem
    Implemented the two-pointer approach to solve the problem of finding the maximum area of water that can be contained between two lines.

--- Arrays/Two_Pointers/Container_With_Most_Water.cpp
+++ Arrays/Two_Pointers/Container_With_Most_Water.cpp
@@ -1,1 +1,25 @@
+// LeetCode 11 - Container With Most Water
+// Approach: Two Pointer
+// TC: O(n)
+// SC: O(1)
 
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int left = 0;
+        int right = n - 1;
+        int maxwater = 0;
+
+        while(left < right) {
+            int width = right - left;
+            int ht = min(height[left], height[right]);
+            int currentwater = width * ht;
+            maxwater = max(maxwater, currentwater);
+
+            height[left] < height[right] ? left++ : right--;
+        }
+
+        return maxwater;
+    }
+};

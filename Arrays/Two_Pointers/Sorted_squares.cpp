c62ba874f8527c2be508370a3bdf93fe1593bf3c
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-19 22:17:08 +0530

    Add sortedSquares function using two-pointer method
    Implement two-pointer approach to return sorted squares of the input array.

--- Arrays/Two_Pointers/Sorted_squares.cpp
+++ Arrays/Two_Pointers/Sorted_squares.cpp
@@ -1,1 +1,33 @@
+// LeetCode 977 - Squares of a Sorted Array
+// Approach: Two Pointer
+// TC: O(n)
+// SC: O(n)
 
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        vector<int> result(n);
+        int index = n - 1;
+
+        while(left <= right) {
+            int leftsq = nums[left] * nums[left];
+            int rightsq = nums[right] * nums[right];
+
+            if(leftsq > rightsq) {
+                result[index] = leftsq;
+                left++;
+            }
+            else {
+                result[index] = rightsq;
+                right--;
+            }
+
+            index--;
+        }
+
+        return result;
+    }
+};

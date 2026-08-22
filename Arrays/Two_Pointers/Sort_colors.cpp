8f21ad80674911e1ad9d90e36eec23a617eda2e8
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-22 10:36:20 +0530

    Update Sort_colors.cpp

--- Arrays/Two_Pointers/Sort_colors.cpp
+++ Arrays/Two_Pointers/Sort_colors.cpp
@@ -1,1 +1,29 @@
+// LeetCode 75 - Sort Colors
+// Approach: Dutch National Flag / Three Pointer
+// TC: O(n)
+// SC: O(1)
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums[high], nums[mid]);
+                high--;
+            }
+        }
+    }
+};

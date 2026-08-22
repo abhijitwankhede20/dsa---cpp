2f1388d324cea54cf6e92572c8b66778fc3a59ce
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-22 10:46:09 +0530

    Add solution for maximum sum subarray of size K
    Implement fixed size sliding window approach to find the maximum sum of a subarray of size K.

--- Arrays/Sliding_Window/Maximum_Sum_Subarray_Size_K.cpp
+++ Arrays/Sliding_Window/Maximum_Sum_Subarray_Size_K.cpp
@@ -1,1 +1,31 @@
+// GFG - Maximum Sum Subarray of Size K
+// Approach: Fixed Size Sliding Window
+// TC: O(n)
+// SC: O(1)
 
+class Solution {
+public:
+    int maximumSumSubarray(vector<int>& arr, int k) {
+        int n = arr.size();
+        int start = 0;
+        int end = 0;
+        int sum = 0;
+        int maxsum = INT_MIN;
+
+        while(end < n) {
+            sum += arr[end];
+
+            if((end - start + 1) < k) {
+                end++;
+            }
+            else if((end - start + 1) == k) {
+                maxsum = max(maxsum, sum);
+                sum -= arr[start];
+                start++;
+                end++;
+            }
+        }
+
+        return maxsum;
+    }
+};

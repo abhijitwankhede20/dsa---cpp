de6a6ec32bbb5a5fcdd8c4882ed7977727e3fe1f
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-22 10:48:48 +0530

    Add solution for Maximum Average Subarray I
    Implemented a fixed-size sliding window approach to find the maximum average of a subarray of size k.

--- Arrays/Sliding_Window/Maximum_Average_Subarray_I.cpp
+++ Arrays/Sliding_Window/Maximum_Average_Subarray_I.cpp
@@ -1,1 +1,31 @@
+// LeetCode 643 - Maximum Average Subarray I
+// Approach: Fixed Size Sliding Window
+// TC: O(n)
+// SC: O(1)
 
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        int start = 0;
+        int end = 0;
+        double sum = 0;
+        double maxaverage = INT_MIN;
+
+        while(end < n) {
+            sum += nums[end];
+
+            if((end - start + 1) < k) {
+                end++;
+            }
+            else if((end - start + 1) == k) {
+                maxaverage = max(maxaverage, sum / k);
+                sum -= nums[start];
+                start++;
+                end++;
+            }
+        }
+
+        return maxaverage;
+    }
+};

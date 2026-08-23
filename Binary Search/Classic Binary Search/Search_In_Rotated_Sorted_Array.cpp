8c3bb32a06bf2ee8cfee9e8928666b842195e557
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-23 23:23:14 +0530

    Add search function for rotated sorted array
    Implemented a modified binary search algorithm to find a target in a rotated sorted array.

--- Binary Search/Classic Binary Search/Search_In_Rotated_Sorted_Array.cpp
+++ Binary Search/Classic Binary Search/Search_In_Rotated_Sorted_Array.cpp
@@ -1,1 +1,35 @@
+// LeetCode 33 - Search in Rotated Sorted Array
+// Approach: Modified Binary Search
+// TC: O(log n)
+// SC: O(1)
 
+class Solution {
+public:
+    int search(vector<int>& nums, int tar) {
+        int n = nums.size();
+        int st = 0;
+        int end = n - 1;
+
+        while(st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if(nums[mid] == tar)
+                return mid;
+
+            if(nums[st] <= nums[mid]) {
+                if(nums[st] <= tar && tar <= nums[mid])
+                    end = mid - 1;
+                else
+                    st = mid + 1;
+            }
+            else {
+                if(nums[mid] <= tar && tar <= nums[end])
+                    st = mid + 1;
+                else
+                    end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};

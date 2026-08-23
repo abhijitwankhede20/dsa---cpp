c0c558ead894dba0ef955e23d093daa8cc8aa4cc
Abhijit Vankhede <abhijitwankhede90@gmail.com>
2026-08-23 23:25:17 +0530

    Add solution for finding minimum in rotated sorted array
    Implemented a modified binary search to find the minimum element in a rotated sorted array.

--- Binary Search/Classic Binary Search/Find_Minimum_In_Rotated_Sorted_Array.cpp
+++ Binary Search/Classic Binary Search/Find_Minimum_In_Rotated_Sorted_Array.cpp
@@ -1,1 +1,26 @@
+// LeetCode 153 - Find Minimum in Rotated Sorted Array
+// Approach: Modified Binary Search
+// TC: O(log n)
+// SC: O(1)
 
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int st = 0;
+        int end = n - 1;
+
+        while(st < end) {
+            int mid = st + (end - st) / 2;
+
+            if(nums[mid] > nums[end]) {
+                st = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+        }
+
+        return nums[st];
+    }
+};
